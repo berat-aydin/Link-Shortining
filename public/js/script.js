24ea38cc38bbeed39ffb063faf3f5e40de3cd105
Berat Aydın <aydinberat352@gmail.com>
2023-04-13 18:45:19 +0300

    edit: script.js

--- public/js/script.js
+++ public/js/script.js
@@ -5,9 +5,16 @@ function scrollFunction() {
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
         document.getElementById("backtotop").style.display = "block";
     }
-
      else {
         document.getElementById("backtotop").style.display = "none";
     }
-
+    
 }
+
+const mainSubmit = document.getElementById("main_submit");
+const mainLink = document.getElementById("main_link");
+
+mainSubmit.addEventListener("click", function(event) {
+    event.preventDefault();
+    mainLink.style.display = "block";
+});
