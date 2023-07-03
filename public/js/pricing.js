23ebfbc47d02f2900acd4cb9ff7b0f3e65a7e5be
Berat Aydın <aydinberat352@gmail.com>
2023-07-04 01:19:00 +0300

    edit

--- public/js/pricing.js
+++ public/js/pricing.js
@@ -1,21 +1,20 @@
 var tableBody = document.getElementById('tabloBody');
+  for (let i = 0; i < 50; i++) {
+    var tableRow = document.createElement('tr');
+      
+    var countryCell = document.createElement('td');
+    var countryImage = document.createElement('img');
+    countryImage.className = 'flags';
+    countryImage.src = '../../images/flags/tr.svg';
+    var countryName = document.createElement('span');
+    countryName.textContent = 'Turkey';
+    countryCell.appendChild(countryImage);
+    countryCell.appendChild(countryName);
+    tableRow.appendChild(countryCell);
     
-    for (let i = 0; i < 50; i++) {
-      var tableRow = document.createElement('tr');
-      
-      var countryCell = document.createElement('td');
-      var countryImage = document.createElement('img');
-      countryImage.className = 'flags';
-      countryImage.src = '../../images/flags/tr.svg';
-      var countryName = document.createElement('span');
-      countryName.textContent = 'Turkey';
-      countryCell.appendChild(countryImage);
-      countryCell.appendChild(countryName);
-      tableRow.appendChild(countryCell);
-      
-      var cpmCell = document.createElement('td');
-      cpmCell.textContent = '40.24 TL';
-      tableRow.appendChild(cpmCell);
-      
-      tableBody.appendChild(tableRow);
-    }
+    var cpmCell = document.createElement('td');
+    cpmCell.textContent = '40.24 TL';
+    tableRow.appendChild(cpmCell);
+    
+    tableBody.appendChild(tableRow);
+  }
